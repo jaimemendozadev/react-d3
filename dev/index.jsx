aa84d52d95105505b84bc84545765a73b80c2e74
jaimemendozadev <jaimemendozadev@gmail.com>
2018-02-26 10:05:35 -0800

    Initial commit
    Setup Server and Frontend

---
+++ dev/index.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => (
+  <div><h1>Welcome to the React/D3 App</h1></div>
+)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const public = path.resolve(__dirname, 'public');
+const dev = path.resolve(__dirname, 'dev/index.jsx');
+
+module.exports = {
+  entry: dev,
+  output: {
+    path: public,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js?x$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  }
+}
