3b52cdf90bd457b0f2b05ae6f0e3859717b10011
jaimemendozadev <jaimemendozadev@gmail.com>
2018-02-26 20:21:36 -0800

    Create Preloader component

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import Preloader from './components/Preloader.jsx';
 
 const App = () => (
   <div><h1>Welcome to the React/D3 App</h1></div>
 )
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<Preloader />, document.querySelector('.container'));
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js?x$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.js?x$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.(png|jpg|gif)$/, use: [{loader: 'file-loader', options: {}}]}
     ]
   }
 }
