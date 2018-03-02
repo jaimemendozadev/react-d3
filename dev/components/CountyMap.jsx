b7c7a4343da8a77b1839d1fdc36bd74b0cc9342a
jaimemendozadev <jaimemendozadev@gmail.com>
2018-03-02 06:06:28 -0800

    Update CountyMap to class component

--- dev/components/CountyMap.jsx
+++ dev/components/CountyMap.jsx
@@ -1,9 +1,40 @@
 import React, {Component} from 'react';
+import * as d3 from 'd3';
+import * as topojson from 'topojson';
+import _ from 'lodash';
+//import County from './County';
 
 
-const CountyMap = () => (
-  <div><h1>CountyMap</h1></div>
-);
+
+class CountyMap extends Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  updateD3(){
+    console.log("updateD3");
+  }
+
+  componentWillReceiveProps(nextProps){
+    console.log("old props", this.props);
+    console.log("nextProps ", nextProps);
+  }
+
+  render(){
+    const {usTopoJson} = this.props;
+
+    if (!usTopoJson){
+      return(
+        null
+      )
+    }
+
+    return(
+      <div>CountyMap</div>
+    )
+  }
+}
 
 export default CountyMap;
 
